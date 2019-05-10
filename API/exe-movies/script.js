69b476cb0c814fb14f84f4282c0e1c92e0f43bac
Flavia Dessoldi <flavia.dessoldi@gmail.com>
2019-05-10 08:34:25 -0300

    movies alterado

--- API/exe-movies/script.js
+++ API/exe-movies/script.js
@@ -20,34 +20,33 @@ fetch('http://www.omdbapi.com/?s='+pesquisa+'&apikey=b5acaa9c')
         const movies = document.querySelector('.movies');
 
         const box = document.createElement('div');
-        box.setAttribute('data-id', '1');
+        box.setAttribute('data-id', filmes.id);
         
         movies.appendChild(box);
 
         const imagem = document.createElement('img');
-                
+        
         box.appendChild(imagem);
-
+        
         imagem.src = filmes.Poster;
 
         const boxDivider = document.createElement('div');
         boxDivider.setAttribute('class', 'box-divider');
-
         box.appendChild(boxDivider);
 
-        const p = document.createElement('p');
+        // const p = document.createElement('p');
 
         const titulo = document.createElement('span');
         titulo.textContent = filmes.Title
-        p.appendChild(titulo);
+        boxDivider.appendChild(titulo);
 
         const ano = document.createElement('span');
-        p.appendChild(ano);
         ano.textContent = filmes.Year
+        boxDivider.appendChild(ano);
 
         const descricao = document.createElement('p');
-        descricao.textContent = 'movie';
-        boxDivider.appendChild(descricao)
+        descricao.textContent = filmes .Type;
+        boxDivider.appendChild(descricao);
 
        
     });
